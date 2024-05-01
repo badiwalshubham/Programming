ab933cc191e7215a2383b9f6bd990b3b32eae6d3
Shubham Badwial <shubhambadiwal8520@gmail.com>
2024-05-01 12:05:42 +0530

    ifelsecodedone

---
+++ ifelse.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+//Write a program thatyy takes an input of age
+// and primnts if you are adult or not
+// >=18, Yes
+// < 18, No
+int main() {
+     int age;
+     cin >> age;
+    if (age>=18)
+    {
+        cout << "Adult";
+    }
+    else
+    {
+        cout << "Not an Adult";
+    }
+    
+}
