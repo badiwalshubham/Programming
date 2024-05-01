22cff0b0cb2dc5b7b49c5ca7cd80935e39d2232e
Shubham Badwial <shubhambadiwal8520@gmail.com>
2024-05-01 12:15:33 +0530

    info_updated

--- learn1.cpp
+++ learn1.cpp
@@ -1,7 +1,14 @@
-#include <iostream>
+// #include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+// using namespace std is the one time solution for the all stds down the code.
+// Also by using the <bits/stdc++.h> will include all the library files in the code.
+// By using Cin you can take the input and by using cout you can give the output.
+// Data types are int, long, long-long, double, string, char and getline mostly used in c++
  int main()
  {
-   std::cout<<"Hey Shubham";
+  //  std::cout<<"Hey Shubham";
+  cout<<"Hey Shubham";
    return 0;
  }
  
